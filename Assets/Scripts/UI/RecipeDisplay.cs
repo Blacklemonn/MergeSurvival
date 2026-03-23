2a4224a24547fec3d84ee07aa5eaa7b76c09f926
Blacklemonn <42985782+Blacklemonn@users.noreply.github.com>
2026-03-23 18:35:16 +0900

    tring add Recipe Window

---
+++ Assets/Scripts/UI/RecipeDisplay.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeDisplay : MonoBehaviour
+{
+    public MergeRecipe mergeRecipe;
+
+    public Sprite[] itemIcons;
+
+    private void Awake()
+    {
+        //아이템의 개수만큼 sprite개수 생성
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Display()
+    {
+        //레시피의 결과, 재료의 아이콘을 디스플레이
+    }
+}
